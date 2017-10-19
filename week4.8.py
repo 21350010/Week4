3df390bc043b78c1d8e81e4f739b13ac1b81532b
21350010 <32065584+21350010@users.noreply.github.com>
2017-10-19 21:13:34 +0100

    Update week4.8.py
    Age integer variable

--- week4.8.py
+++ week4.8.py
@@ -1,6 +1,6 @@
 name=input("Enter your name :")
 #name string variable
 age=int(input("Enter your age :"))
-
+#age integer variable
 print("Hello",name,"next year you will be",age,"years old!")
         
